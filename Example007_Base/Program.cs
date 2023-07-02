ba70aea5319834151f9a831a1a9d01cbe5c8b4fe
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-03 00:07:10 +0500

    Example007 Base

---
+++ Example007_Base/Program.cs
@@ -0,0 +1,9 @@
+﻿int a =1, b =8, c =32, d =10, e =4;
+int max = a;
+if( a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
