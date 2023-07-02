79d548e0241f5e8d0b6de46b6e262c6aabceb307
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-02 23:59:19 +0500

    Example006 IfElse

---
+++ Example006_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите свое имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "сергей")
+{
+    Console.WriteLine("Ура, это же Сергей!");
+}
+else
+{
+    Console.WriteLine("Привет " + username);
+}
