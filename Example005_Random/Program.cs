1cffa2894121a1f0ecdce38518e22d87b8dc8586
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-02 21:53:15 +0500

    Example005 Random

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = new Random().Next(0,100);// любое целое число в диапозоне 0 до 99
+Console.WriteLine(numberA); 
+int numberB = new Random().Next(0,100);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
