0e0536ea9c28d899d3c7c2e34f66360347e4d064
SergeyCherevichenko <cherevichenkosn@gmail.com>
2023-07-03 12:42:01 +0500

    Example008 Dog and Two Friends follow each other

--- Example008_DogAndTwoFriends/Program.cs
+++ Example008_DogAndTwoFriends/Program.cs
@@ -1,15 +1,15 @@
 ﻿int count = 0;
-double distance  = 100;
+double distance  = 20;
 double firstFriendSpeed = 1;
 double secondFriendSpeed = 2;
 double dogSpeed = 5;
-int friend = 2;
+int friend = 1;
 double time;
 while (distance > 10 )
 {
     if (friend == 1)
     {
-        time = distance / (firstFriendSpeed + dogSpeed);
+        time = distance / (dogSpeed - firstFriendSpeed);
         friend = 2;
     }
     else
@@ -17,7 +17,7 @@ while (distance > 10 )
         time = distance / (secondFriendSpeed + dogSpeed);
         friend = 1;
     }
-    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    distance = distance - (secondFriendSpeed - firstFriendSpeed) * time;
     count = count +1;
 }
 Console.WriteLine("Собака пробежала " + count);
